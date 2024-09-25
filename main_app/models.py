097f8858e6d3bcdc685e0b4cdd831271b87e14cf
aallen417 <austin.allen417@gmail.com>
2024-09-25 13:28:01 -0400

    adds car model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+  make = models.CharField(max_length=25)
+  model = models.CharField(max_length=25)
+  description = models.TextField(max_length=250)
+  year = models.IntegerField()
+
+  def __str__(self):
+      return self.name
